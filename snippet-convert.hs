819aedea3521c07cfcd94cc2d94256496b40a3d3
andys8 <andys8@users.noreply.github.com>
2018-11-18 22:05:45 +0100

    Travis file with stack interpreting a file

--- snippet-convert.hs
+++ snippet-convert.hs
@@ -1,5 +1,5 @@
 #!/usr/bin/env stack
--- stack --resolver lts-10.2 script
+-- stack --resolver lts-10.2 --install-ghc runghc --package turtle
 {-# LANGUAGE OverloadedStrings #-}
 
 import           Data.Maybe
