20060a3da7d33dac272265da467b3b1d00b3b186
andys8 <andys8@users.noreply.github.com>
2018-11-18 17:02:21 +0100

    Formatting with hfmt

--- snippet-convert.hs
+++ snippet-convert.hs
@@ -1,26 +1,25 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-10.2 script
-
 {-# LANGUAGE OverloadedStrings #-}
 
-import Turtle
-import Debug.Trace (trace, traceShow, traceShowId, traceShowM)
-import Data.Maybe
-import Data.Text
+import           Data.Maybe
+import           Data.Text
+import           Debug.Trace (trace, traceShow, traceShowId, traceShowM)
+import           Turtle
 
 main :: MonadIO io => io ()
 main = do
   args <- arguments
   case listToMaybe args of
     Just file -> do
-        fileExists <- testfile $ fromText file
-        if not fileExists then die "File doesn't exist"
+      fileExists <- testfile $ fromText file
+      if not fileExists
+        then die "File doesn't exist"
         else do
           fileContent <- liftIO $ readTextFile $ fromText file
           mapM_ echo $ textToLines $ pack $ convertSnippets $ unpack fileContent
           return ()
     Nothing -> die "Filename has to be provided as param"
-
 
 convertSnippets :: String -> String
 convertSnippets lines = lines
